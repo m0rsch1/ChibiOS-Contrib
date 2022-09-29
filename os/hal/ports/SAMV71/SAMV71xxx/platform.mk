2f3840d4ab2d193ac6c2179e0c6f8cf2d7045158
Moritz Schilling <moritz.schilling@dfki.de>
2022-09-29 14:51:49 +0200

    Started development of SAMV71 HAL layer

---
+++ os/hal/ports/SAMV71/SAMV71xxx/platform.mk
@@ -0,0 +1,32 @@
+# Required source files
+PLATFORMSRC := ${CHIBIOS}/os/hal/ports/common/ARMCMx/nvic.c \
+	       ${CHIBIOS_CONTRIB}/os/hal/ports/SAMV71/SAMV71xxx/hal_lld.c \
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/ports/common/ARMCMx \
+              ${CHIBIOS_CONTRIB}/os/hal/ports/SAMV71/SAMV71xxx
+
+# List of all the template platform files.
+ifeq ($(USE_SMART_BUILD),yes)
+
+# Configuration files directory
+ifeq ($(HALCONFDIR),)
+  ifeq ($(CONFDIR),)
+    HALCONFDIR = .
+  else
+    HALCONFDIR := $(CONFDIR)
+  endif
+endif
+
+HALCONF := $(strip $(shell cat $(CONFDIR)/halconf.h | egrep -e "\#define"))
+
+else
+endif
+
+# Drivers compatible with the platform.
+include $(CHIBIOS_CONTRIB)/os/hal/ports/SAMV71/LLD/SYSTICKv1/driver.mk
+#include $(CHIBIOS)/os/hal/ports/SAMV71/LLD/GPIOv1/driver.mk
+
+# Shared variables
+ALLCSRC += $(PLATFORMSRC)
+ALLINC  += $(PLATFORMINC)
