60dda70b3aa97bc457c3d331a6ab461417899c7f
Moritz Schilling <moritz.schilling@dfki.de>
2022-10-05 15:04:59 +0200

    Disable the INITIALLY enabled watchdog!
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- demos/SAMV71/RT-SAMV71Q21-XPLAINED-ULTRA/board.c
+++ demos/SAMV71/RT-SAMV71Q21-XPLAINED-ULTRA/board.c
@@ -1,6 +1,10 @@
 #include "board.h"
+#include "samv71.h"
 
 void boardInit(void)
 {
+    /* Disable the watchdog */
+    WDT->WDT_MR = WDT_MR_WDDIS;
     SystemInit();
+    SystemCoreClockUpdate();
 }
