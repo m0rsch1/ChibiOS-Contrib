eab4dd8870e025ee8cb6035c5d3c7b4b325686f1
Moritz Schilling <moritz.schilling@dfki.de>
2022-11-28 10:43:05 +0100

    Added GPT port. To be tested
    Signed-off-by: Moritz Schilling <moritz.schilling@dfki.de>

--- os/hal/ports/SAMV71/LLD/GPTv1/hal_gpt_lld.c
+++ os/hal/ports/SAMV71/LLD/GPTv1/hal_gpt_lld.c
@@ -35,10 +35,10 @@
 /*===========================================================================*/
 
 /**
- * @brief   GPTD1 driver identifier.
+ * @brief   GPTD0 driver identifier.
  */
-#if (PLATFORM_GPT_USE_GPT1 == TRUE) || defined(__DOXYGEN__)
-GPTDriver GPTD1;
+#if (PLATFORM_GPT_USE_GPT0 == TRUE) || defined(__DOXYGEN__)
+GPTDriver GPTD0;
 #endif
 
 /*===========================================================================*/
@@ -53,6 +53,33 @@ GPTDriver GPTD1;
 /* Driver interrupt handlers.                                                */
 /*===========================================================================*/
 
+#if PLATFORM_GPT_USE_GPT0
+/**
+ * @brief   TC0 IRQ Handler
+ *
+ * @isr
+ */
+OSAL_IRQ_HANDLER(TC0_HANDLER) {
+
+  OSAL_IRQ_PROLOGUE();
+
+  gpt_lld_serve_interrupt(&GPTD0);
+
+  OSAL_IRQ_EPILOGUE();
+}
+#endif
+
+void gpt_lld_serve_interrupt(GPTDriver *gptp)
+{
+    uint32_t sr = gptp->driver->TC_CHANNEL[gptp->channel].TC_SR;
+    sr &= gptp->driver->TC_CHANNEL[gptp->channel].TC_IMR;
+    if (sr & TC_SR_CPCS)
+    {
+        // Trigger on RC compare occurred. Call callback
+        _gpt_isr_invoke_cb(gptp);
+    }
+}
+
 /*===========================================================================*/
 /* Driver exported functions.                                                */
 /*===========================================================================*/
@@ -64,9 +91,11 @@ GPTDriver GPTD1;
  */
 void gpt_lld_init(void) {
 
-#if PLATFORM_GPT_USE_GPT1 == TRUE
+#if PLATFORM_GPT_USE_GPT0 == TRUE
   /* Driver initialization.*/
-  gptObjectInit(&GPTD1);
+  gptObjectInit(&GPTD0);
+  GPTD0.driver = TC0;
+  gpt_lld_stop_timer(&GPTD0);
 #endif
 }
 
@@ -81,9 +110,35 @@ void gpt_lld_start(GPTDriver *gptp) {
 
   if (gptp->state == GPT_STOP) {
     /* Enables the peripheral.*/
-#if PLATFORM_GPT_USE_GPT1 == TRUE
-    if (&GPTD1 == gptp) {
-
+#if PLATFORM_GPT_USE_GPT0 == TRUE
+    if (&GPTD0 == gptp) {
+        // First enable the clock of the timer
+        pmc_enable_periph_clk(ID_TC0);
+        // Enable the NVIC
+        nvicEnableVector(TC0_NVIC_NUMBER, GPT_NVIC_PRIORITY);
+        // Disable write protection
+        gptp->driver->TC_WPMR = TC_WPMR_WPKEY_PASSWD;
+        // Given the requested frequency and the different clock options, we should derive the best clock to use for the timer
+        int selected_divisor = 0;
+        const uint32_t divisors[] = {2, 8, 32, 128};
+        for (int i = 0; i < 4; i++)
+        {
+            // Select the current divisor
+            selected_divisor = i;
+            // Calculate boundaries of frequencies
+            const uint32_t hf = GPT_MAIN_CLK / divisors[i];
+            const uint32_t lf = hf / 65535;
+            if ((gptp->config->frequency < hf) && (gptp->config->frequency > lf))
+                break;
+        }
+        // Set CMR to zero first
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_CMR = 0;
+        // Set the clock divisor (by index not by value)
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_CMR |= selected_divisor;
+        // Set the timer to use reset the counter on RC compare
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_CMR |= TC_CMR_CPCTRG;
+        
+        // NOTE: IRQs are enabled when the timer is started
     }
 #endif
   }
@@ -104,9 +159,21 @@ void gpt_lld_stop(GPTDriver *gptp) {
     /* Resets the peripheral.*/
 
     /* Disables the peripheral.*/
-#if PLATFORM_GPT_USE_GPT1 == TRUE
-    if (&GPTD1 == gptp) {
-
+#if PLATFORM_GPT_USE_GPT0 == TRUE
+    if (&GPTD0 == gptp) {
+        // Disable write protection
+        gptp->driver->TC_WPMR = TC_WPMR_WPKEY_PASSWD;
+        // Disable interrupts and reset the status register
+        /*  Disable TC clock. */
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_CCR = TC_CCR_CLKDIS;
+        /*  Disable interrupts. */
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_IDR = 0xFFFFFFFF;
+        /*  Clear status register. */
+        gptp->driver->TC_CHANNEL[gptp->channel].TC_SR;
+        // Disable the NVIC
+        nvicDisableVector(TC0_NVIC_NUMBER);
+        // Disable the clock of the timer
+        pmc_disable_periph_clk(ID_TC0);
     }
 #endif
   }
@@ -121,10 +188,15 @@ void gpt_lld_stop(GPTDriver *gptp) {
  * @notapi
  */
 void gpt_lld_start_timer(GPTDriver *gptp, gptcnt_t interval) {
-
-  (void)gptp;
-  (void)interval;
-
+    // TODO: In the STM32 implementation this function does not check the state. Is this correct?
+    // Change the interval
+    gpt_lld_change_interval(gptp, interval);
+    // Start the clock and the timer
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_CCR = TC_CCR_CLKEN | TC_CCR_SWTRG;
+    /*  Clear status register. */
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_SR;
+    // Enable RC compare IRQ
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_IER = TC_IER_CPCS;
 }
 
 /**
@@ -135,9 +207,12 @@ void gpt_lld_start_timer(GPTDriver *gptp, gptcnt_t interval) {
  * @notapi
  */
 void gpt_lld_stop_timer(GPTDriver *gptp) {
-
-  (void)gptp;
-
+    // Stop the clock
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_CCR = TC_CCR_CLKDIS;
+    /*  Clear status register. */
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_SR;
+    // Disable RC compare IRQ (and all other enabled IRQs)
+    gptp->driver->TC_CHANNEL[gptp->channel].TC_IDR = gptp->driver->TC_CHANNEL[gptp->channel].TC_IMR;
 }
 
 /**
--- os/hal/ports/SAMV71/LLD/GPTv1/hal_gpt_lld.h
+++ os/hal/ports/SAMV71/LLD/GPTv1/hal_gpt_lld.h
@@ -27,9 +27,18 @@
 
 #if (HAL_USE_GPT == TRUE) || defined(__DOXYGEN__)
 
+#include "hal_st_lld.h"
+
 /*===========================================================================*/
 /* Driver constants.                                                         */
 /*===========================================================================*/
+
+#define GPT_MAIN_CLK (CHIP_FREQ_CPU_MAX / 2)
+
+// NOTE: USART has 6, SYSTICK has 2
+#define GPT_NVIC_PRIORITY 4
+#define TC0_HANDLER Vector9C
+#define TC0_NVIC_NUMBER TC0_IRQn
 
 /*===========================================================================*/
 /* Driver pre-compile time settings.                                         */
@@ -40,18 +49,26 @@
  * @{
  */
 /**
- * @brief   GPTD1 driver enable switch.
- * @details If set to @p TRUE the support for GPTD1 is included.
+ * @brief   GPTD0 driver enable switch.
+ * @details If set to @p TRUE the support for GPTD0 is included.
  * @note    The default is @p FALSE.
  */
-#if !defined(PLATFORM_GPT_USE_GPT1) || defined(__DOXYGEN__)
-#define PLATFORM_GPT_USE_GPT1               FALSE
+#if !defined(PLATFORM_GPT_USE_GPT0) || defined(__DOXYGEN__)
+#define PLATFORM_GPT_USE_GPT0               FALSE
 #endif
 /** @} */
 
 /*===========================================================================*/
 /* Derived constants and error checks.                                       */
 /*===========================================================================*/
+
+#if (GPT_NVIC_PRIORITY < ST_NVIC_PRIORITY)
+#warning "Setting GPT prio higher than systick prio might cause problems"
+#endif
+
+#if PLATFORM_GPT_USE_GPT0 && !defined(ID_TC0)
+#error "GPT0 is not present on this device"
+#endif
 
 /*===========================================================================*/
 /* Driver data structures and types.                                         */
@@ -102,6 +119,10 @@ struct GPTDriver {
   GPT_DRIVER_EXT_FIELDS
 #endif
   /* End of the mandatory fields.*/
+  /* Pointer to the driver instance */
+  Tc*                       driver;
+  /* Index to the channel used for this GPT */
+  uint8_t                   channel;
 };
 
 /*===========================================================================*/
@@ -121,17 +142,18 @@ struct GPTDriver {
  * @param[in] interval  new cycle time in timer ticks
  * @notapi
  */
-#define gpt_lld_change_interval(gptp, interval) {                           \
-  (void)gptp;                                                               \
-  (void)interval;                                                           \
-}
+#define gpt_lld_change_interval(gptp, interval) (gptp->driver->TC_CHANNEL[gptp->channel].TC_RC = (gptcnt_t)interval)
+
+#define gpt_lld_get_interval(gptp) ((gptcnt_t)gptp->driver->TC_CHANNEL[gptp->channel].TC_RC)
+
+#define gpt_lld_get_counter(gptp) ((gptcnt_t)gptp->driver->TC_CHANNEL[gptp->channel].TC_CV)
 
 /*===========================================================================*/
 /* External declarations.                                                    */
 /*===========================================================================*/
 
-#if (PLATFORM_GPT_USE_GPT1 == TRUE) && !defined(__DOXYGEN__)
-extern GPTDriver GPTD1;
+#if (PLATFORM_GPT_USE_GPT0 == TRUE) && !defined(__DOXYGEN__)
+extern GPTDriver GPTD0;
 #endif
 
 #ifdef __cplusplus
@@ -143,6 +165,7 @@ extern "C" {
   void gpt_lld_start_timer(GPTDriver *gptp, gptcnt_t interval);
   void gpt_lld_stop_timer(GPTDriver *gptp);
   void gpt_lld_polled_delay(GPTDriver *gptp, gptcnt_t interval);
+  void gpt_lld_serve_interrupt(GPTDriver *gptp);
 #ifdef __cplusplus
 }
 #endif
