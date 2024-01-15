8223ae45b9e092af4eba8d3d1e622228d519266e
Pierre Willenbrock <pierre.willenbrock@dfki.de>
2024-01-15 13:31:47 +0100

    samv71 pmc: add generic and programmable clocks, utmi-pll
    Basically clocks with multiple sources and their own divider; very
    similar to each other, but named separately in the documentation.
    
    The utmi pll provides a 480MHz clock for the HighSpeed USB UTMI
    that can then be divided down to e.g. 8MHz for CAN, while
    the main 300MHz clock cannot. Downside of the utmi pll is
    the limited multipliers: 30 and 40, limiting the input clock
    to 12 or 16 MHz.

--- os/hal/ports/SAMV71/LLD/PMCv1/hal_pmc_lld.c
+++ os/hal/ports/SAMV71/LLD/PMCv1/hal_pmc_lld.c
@@ -1,5 +1,6 @@
 #include "hal_pmc_lld.h"
 
+#include "system_samv71.h"
 /**
  * \brief Enable the specified peripheral clock.
  *
@@ -64,4 +65,93 @@ uint32_t pmc_disable_periph_clk(uint32_t ul_id)
 
         return 0;
 #endif /* defined(REG_PMC_PCR) */
+}
+
+uint32_t pmc_enable_generic_clk(uint32_t ul_id)
+{
+    PMC->PMC_PCR = PMC_PCR_PID(ul_id);
+    uint32_t pcr = PMC->PMC_PCR;
+    pcr |= PMC_PCR_CMD;
+    pcr |= PMC_PCR_GCLKEN;
+    PMC->PMC_PCR = pcr;
+    return 0;
+}
+
+uint32_t pmc_disable_generic_clk(uint32_t ul_id)
+{
+    PMC->PMC_PCR = PMC_PCR_PID(ul_id);
+    uint32_t pcr = PMC->PMC_PCR;
+    pcr |= PMC_PCR_CMD;
+    pcr &= ~PMC_PCR_GCLKEN;
+    PMC->PMC_PCR = pcr;
+    return 0;
+}
+
+uint32_t pmc_configure_generic_clk(uint32_t ul_id, uint32_t source, uint32_t divider)
+{
+    PMC->PMC_PCR = PMC_PCR_PID(ul_id);
+    uint32_t pcr = PMC->PMC_PCR;
+    pcr |= PMC_PCR_CMD;
+    pcr &= ~(PMC_PCR_GCLKCSS_Msk | PMC_PCR_GCLKDIV_Msk);
+    pcr |= source & PMC_PCR_GCLKCSS_Msk;
+    pcr |= PMC_PCR_GCLKDIV(divider+1);
+    PMC->PMC_PCR = pcr;
+    return 0;
+}
+
+uint32_t pmc_enable_programmable_clk ( uint32_t pck_id )
+{
+    if(pck_id >= 8)
+        return -1;
+    PMC->PMC_SCER = PMC_SCER_PCK0 << pck_id;
+    ProgrammableClockUpdate(pck_id);
+    return 0;
+}
+
+uint32_t pmc_disable_programmable_clk ( uint32_t pck_id )
+{
+    if(pck_id >= 8)
+        return -1;
+    PMC->PMC_SCDR = PMC_SCDR_PCK0 << pck_id;
+    ProgrammableClockUpdate(pck_id);
+    return 0;
+}
+
+uint32_t pmc_configure_programmable_clk ( uint32_t pck_id, uint32_t source, uint32_t divider )
+{
+    if(pck_id >= 8 || divider <= 0 || divider > 256)
+        return -1;
+    PMC->PMC_PCK[pck_id] = (PMC_PCK_CSS_Msk & source) | PMC_PCK_PRES(divider-1);
+    ProgrammableClockUpdate(pck_id);
+    return 0;
+}
+
+#define SYS_BOARD_UPLLCOUNT CKGR_PLLAR_PLLACOUNT((300*CHIP_FREQ_SLCK_RC_MAX+1000000-1)/1000000)
+
+uint32_t pmc_enable_utmi_clk()
+{
+    PMC->CKGR_UCKR = CKGR_UCKR_UPLLCOUNT(SYS_BOARD_UPLLCOUNT) | CKGR_UCKR_UPLLEN;
+    return 0;
+}
+
+uint32_t pmc_disable_utmi_clk()
+{
+    PMC->CKGR_UCKR = 0;
+    for(int pck_id = 0; pck_id < 8; pck_id++) {
+        ProgrammableClockUpdate(pck_id);
+    }
+    return 0;
+}
+
+uint32_t pmc_configure_utmi_clk ( uint32_t freq_mode )
+{
+    if(freq_mode == ~0U) {
+        freq_mode = SAMV71_MAINCLK == 12000000?UTMI_CKTRIM_FREQ_XTAL12:
+                    UTMI_CKTRIM_FREQ_XTAL16;
+    }
+    UTMI->UTMI_CKTRIM = freq_mode;
+    for(int pck_id = 0; pck_id < 8; pck_id++) {
+        ProgrammableClockUpdate(pck_id);
+    }
+    return 0;
 }
--- os/hal/ports/SAMV71/LLD/PMCv1/hal_pmc_lld.h
+++ os/hal/ports/SAMV71/LLD/PMCv1/hal_pmc_lld.h
@@ -5,3 +5,32 @@
 
 uint32_t pmc_enable_periph_clk(uint32_t ul_id);
 uint32_t pmc_disable_periph_clk(uint32_t ul_id);
+
+uint32_t pmc_enable_generic_clk(uint32_t ul_id);
+uint32_t pmc_disable_generic_clk(uint32_t ul_id);
+
+/**
+ * @param source one of PMC_PCR_GCLKCSS_SLOW_CLK, PMC_PCR_GCLKCSS_MAIN_CLK,
+ *   PMC_PCR_GCLKCSS_PLLA_CLK, PMC_PCR_GCLKCSS_UPLL_CLK, PMC_PCR_GCLKCSS_MCK_CLK
+ * @param divider 1-256
+ */
+uint32_t pmc_configure_generic_clk(uint32_t ul_id, uint32_t source, uint32_t divider);
+
+uint32_t pmc_enable_programmable_clk(uint32_t pck_id);
+uint32_t pmc_disable_programmable_clk(uint32_t pck_id);
+
+/**
+ * @param source one of PMC_PCK_CSS_SLOW_CLK, PMC_PCK_CSS_MAIN_CLK,
+ *  PMC_PCK_CSS_PLLA_CLK, PMC_PCK_CSS_UPLL_CLK, PMC_PCK_CSS_MCK
+ * @param divider 1-256
+ */
+uint32_t pmc_configure_programmable_clk(uint32_t pck_id, uint32_t source, uint32_t divider);
+
+uint32_t pmc_enable_utmi_clk(void);
+uint32_t pmc_disable_utmi_clk(void);
+
+/**
+ * @param freq_mode one of UTMI_CKTRIM_FREQ_XTAL12, UTMI_CKTRIM_FREQ_XTAL16 for
+ *                  PLL frequency multiplication of x40 and x30.
+ */
+uint32_t pmc_configure_utmi_clk(uint32_t freq_mode);
